4ab46e9ac62b0abeff5eafbf979f220f4bae15e0
yitingw <gigiagatha233@gmail.com>
2018-05-16 14:22:05 +0800

    debug

--- dust/config.py
+++ dust/config.py
@@ -4,8 +4,7 @@ class Config(object):
     SQLALCHEMY_ECHO = True
     REDIS_URL = "redis://localhost:6379/0"
     LOGIN_EXPIRE_TIME = 7200*12
-    GH_CLIENT_ID = '153a7fb787d1bd541839'
-    GH_CLIENT_SECRET = 'ca6ea91331f6b8de9620418dbe1a84f491caaa64'
+
 
 class DevConfig(Config):
     DEBUG = True
