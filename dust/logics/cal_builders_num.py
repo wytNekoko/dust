33e1dfa41d72d0613172bc9ba2b4570c6e805e17
yitingw <gigiagatha233@gmail.com>
2018-05-20 23:37:35 +0800

    bugfix

--- dust/logics/cal_builders_num.py
+++ dust/logics/cal_builders_num.py
@@ -1,10 +1,21 @@
 from ..core import db
-from ..models.user_planet import Planet, BuildRecord
+from ..models.user_planet import Planet, BuildRecord, User
 
 
 def cal_builders():
     records = BuildRecord.query.all()
     for re in records:
+        re.dust_num = int(re.dust_num/8.8)
+        db.session.flush()
         p = Planet.query.get(re.planet_id)
+        p.dust_num += re.dust_num
         p.builder_num += 1
+        db.session.flush()
+        re.planet_dust = p.dust_num
+        db.session.flush()
+    ps = Planet.query.all()
+    for p in ps:
+        u = User.query.get(p.owner_id)
+        u.planet_dust_sum += p.dust_num
+        db.session.flush()
     db.session.commit()
